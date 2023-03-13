3c71d6fedd2e1d3931399aaab9e04f7282c0aba1
Henrik-Bengtsson <112540515+Henrik-Bengtsson@users.noreply.github.com>
2023-03-13 17:01:14 +0100

    Initial commit

---
+++ src/main/java/com/example/carthief/CarthiefApplication.java
@@ -0,0 +1,13 @@
+package com.example.carthief;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarthiefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarthiefApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carthief/CarthiefApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.carthief;
+
+//@SpringBootTest
+//class CarthiefApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
