05f73ba4f543276eefd3db2bd83e0baf8c96b34b
RobertMili <robertmilicevic3869@gmail.com>
2023-03-29 20:10:05 +0200

    making sonarCloud happy

--- src/main/java/com/example/carthief/CarThiefApplication.java
+++ src/main/java/com/example/carthief/CarThiefApplication.java
@@ -1,10 +1,8 @@
 package com.example.carthief;
 
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class CarThiefApplication {
