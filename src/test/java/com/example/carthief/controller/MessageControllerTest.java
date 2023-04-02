b1154fee242dd8b3bf484e5947ec9daf27fce8f7
RobertMili <robertmilicevic3869@gmail.com>
2023-04-02 22:25:27 +0200

    ignore security oauth2

--- src/test/java/com/example/carthief/controller/MessageControllerTest.java
+++ src/test/java/com/example/carthief/controller/MessageControllerTest.java
@@ -4,6 +4,7 @@ import com.example.carthief.service.Publisher;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -15,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @WebMvcTest(controllers = MessageController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class MessageControllerTest {
 
     @Autowired
