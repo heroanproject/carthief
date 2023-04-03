b5588806e1470bcea6d479af450c75428338a293
Henrik-Bengtsson <112540515+Henrik-Bengtsson@users.noreply.github.com>
2023-04-03 18:37:15 +0200

    Add user class

--- src/main/java/com/example/carthief/security/SecurityConfig.java
+++ src/main/java/com/example/carthief/security/SecurityConfig.java
@@ -28,7 +28,6 @@ public class SecurityConfig {
                 .httpBasic()
                 .and()
                 .build();
-
     }
 
     @Bean
---
+++ src/main/java/com/example/carthief/security/User.java
@@ -0,0 +1,47 @@
+package com.example.carthief.security;
+
+import com.mongodb.lang.NonNull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Document(collation = "user")
+public class User {
+
+    @Id
+    private Long id;
+
+    @NonNull
+    @Column(unique = true)
+    private String name;
+
+    @NonNull
+    private String password;
+
+    @NonNull
+    private Role role;
+
+    public User(@NonNull String name, @NonNull String password, @NonNull Role role) {
+        this.name = name;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(password, user.password) && role == user.role;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, password, role);
+    }
+}
